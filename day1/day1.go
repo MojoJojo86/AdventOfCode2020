80f2b0253365b3a6f3d03c3b919018691d6a563f
joe bradshaw <joseph.bradshaw@autotrader.co.uk>
2020-12-01 14:22:06 +0000

    Added solution for part 2

--- day1/day1.go
+++ day1/day1.go
@@ -23,12 +23,27 @@ func main() {
 		values = append(values, scanner.Text())
 	}
 
+	// Part 1 Solution
 	for i := 0; i < len(values); i++ {
 		for j := i + 1; j < len(values); j++ {
 			firstNum, _ := strconv.Atoi(string(values[i]))
 			secondNum, _ := strconv.Atoi(string(values[j]))
 			if firstNum+secondNum == 2020 {
 				fmt.Printf("first: %d second: %d, overall: %d", firstNum, secondNum, firstNum*secondNum)
+			}
+		}
+	}
+
+	// Part 2 Solution 
+	for i := 0; i < len(values); i++ {
+		for j := i + 1; j < len(values); j++ {
+			for k := j + 1; k < len(values); k++ {
+				firstNum, _ := strconv.Atoi(string(values[i]))
+				secondNum, _ := strconv.Atoi(string(values[j]))
+				thirdNum, _ := strconv.Atoi(string(values[k]))
+				if firstNum+secondNum+thirdNum == 2020 {
+					fmt.Printf("first: %d second: %d, third: %d, overall: %d", firstNum, secondNum, thirdNum, firstNum*secondNum*thirdNum)
+				}
 			}
 		}
 	}
